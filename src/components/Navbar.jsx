c908083d65354f9794a5fd8d97ec153d92ef46fe
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-19 19:57:00 -0400

    Created Nav Element and added CSS Styles

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Navbar(){
+    return(
+        <>
+        <nav>
+            <h2>About</h2>
+            <h2>Home</h2>
+            <h2>Projects</h2>
+
+        </nav>
+        </>
+
+    )
+
+
+}
