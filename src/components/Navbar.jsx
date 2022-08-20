f87c908dea5ad8a67d5443a50ba880201c0e724a
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-20 17:26:14 -0400

    Nav Bar Improvements + CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,16 @@ import React from "react";
 
 export default function Navbar(){
     return(
-        <>
+        <div id="navbox">
         <nav>
+            <h2 id="special">Hussam Baradei</h2>
             <h2>About</h2>
             <h2>Home</h2>
             <h2>Projects</h2>
 
         </nav>
-        </>
+        
+        </div>
 
     )
 
