a044cee6bb5b8e4ebaf9a45bbe2198aaf792c739
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-22 10:37:41 -0400

    Added an About section; contains details about me

---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+
+export default function About(){
+    return(
+        <div id="second">
+            <div id="portrait">
+            <img src="https://i.pinimg.com/originals/d9/4a/49/d94a495eca526d82ebbe0640aea413a9.jpg"/>
+            </div>
+
+            <div id="content">
+            <h3>About me:</h3>
+        <h4>Hi, my name is Hussam Baradei I'm currently a second year student, student studying Computer Science at York University</h4>
+        <button>LinkedIn</button>
+        <p>About Me:</p>
+    
+            </div>
+
+   
+        </div>
+
+
+
+
+    )
+
+
+}
