70f79fd3a96fe0418d329833563e2537f02a7197
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-22 10:26:14 -0400

    Added a typing effect on the intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,13 +3,28 @@ import React, { useState } from "react";
 
 export default function Intro(){
     const [typingeffect, setTyping] = useState("") 
+    let i = -1;
+    let txt = 'Welcome to my portfalio website!'; /* The text */
+    let speed = 100;
+    let a= false;
+    
+    if(typingeffect=="" &&a==false){
+        a=true;
+        typeWriter();
+    }
    
-
+    function typeWriter() {
+        if (typingeffect.length < txt.length) {
+            setTyping(prevstate=> prevstate+txt.charAt(i))
+          i++;
+          setTimeout(typeWriter, speed); 
+        }
+      }
 
     return(
         <div id="intro">
             <h1>Hey</h1>
-            <h1>I'm Hussam</h1>
+            <h1>I'm <mark id="hi" style={{color:typingeffect==txt?"Blue":"Black"}}>Hussam</mark></h1>
            <h2>{typingeffect}</h2>
         </div>
     )
