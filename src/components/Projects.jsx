b1328b053feef44eaa49aaaae892e4989780fb55
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-23 15:23:26 -0400

    Finished Project List formating

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,13 +6,11 @@ return(
     <>
 
         <div id="projectx">
-            <h4>Ho</h4>
-        </div>
-        <div id="projectx">
-            <h4>Ho</h4>
-        </div>
-        <div id="projectx">
-            <h4>Ho</h4>
+            <h5>Weather App</h5>
+            <section>
+            <img className="img2" src="https://i.pinimg.com/originals/d9/4a/49/d94a495eca526d82ebbe0640aea413a9.jpg"/>
+            </section>
+            <a href='www.google.ca'><button className="github">View On Github</button></a>
         </div>
     </>
 )
