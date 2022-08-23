7108c8b52def901d1192a2a5fe99e0f6b12668d4
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-23 13:06:03 -0400

    Added more info + details to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 
-
+import photo from '../assets/portrait_edited.png'
 
 export default function About(){
     return(
         <div id="second">
-            <div id="portrait">
-            <img src="https://i.pinimg.com/originals/d9/4a/49/d94a495eca526d82ebbe0640aea413a9.jpg"/>
-            </div>
+         <div id="portrait">
+         <img src={photo}/>
+         </div>
 
             <div id="content">
+            <h3>Hussam Baradei</h3>
+            <h4>Front End Dev</h4>
             <h3>About me:</h3>
         <h4>Hi, my name is Hussam Baradei I'm currently a second year student, student studying Computer Science at York University</h4>
-        <button>LinkedIn</button>
-        <p>About Me:</p>
-    
+        <h3>Interests:</h3>
+        <h4>Outside of my field of study I'm really intersted in traveling. So far I’ve been to 2 out of the 7 wonders of the world.  My goal is to travel to all 7 throughout my lifetime. I also like to go out of my comfort zone and try new things, in a sense being a thrill seeker.</h4>
+       
+        <h3>Coding Languages:</h3>
+        <h4>-Java</h4>
+        <h4>-Java</h4>
+        <a href="www.google.ca"><button>LinkedIn</button></a>
             </div>
 
    
