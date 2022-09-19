3723dace5a6a71205d6fe25afe045898582cce7d
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-09-19 12:19:05 -0400

    Edited Interests section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,9 +14,9 @@ export default function About(){
             <h4>hussam.baradei@yahoo.com</h4>
             <a href='https://drive.google.com/drive/folders/1QLBi5Xwqfq5fndMmuKvVJYlcsqORQEIn?usp=sharing' target="_blank"><h4>View Resume</h4></a>
             <h3>About me:</h3>
-        <h4>Hi, my name is Hussam Baradei I'm currently a second year student, student studying Computer Science at York University</h4>
+        <h4>Hi, my name is Hussam Baradei I'm currently a second year student, studying Computer Science at York University</h4>
         <h3>Interests:</h3>
-        <h4>Outside of my field of study I'm really intersted in traveling. So far I’ve been to 2 out of the 7 wonders of the world.  My goal is to travel to all 7 throughout my lifetime. I also like to go out of my comfort zone and try new things, in a sense being a thrill seeker.</h4>
+        <h4>Outside of my field of study I'm really interested in traveling. So far I’ve been to 2 out of the 7 wonders of the world.  My goal is to travel to all 7 throughout my lifetime. I also like to go out of my comfort zone and try new things, whether it's sky diving or something more laid back I'll give it a go</h4>
        
         
         <a href="https://www.linkedin.com/in/hussam-baradei-2b671423b/" target="_blank"><button className="linkedin">LinkedIn</button></a>
