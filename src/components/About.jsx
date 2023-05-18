a8ea6a27761f7cb5fd26411ef059f01677b5f382
ToxicTerror21 <hussam.baradei@yahoo.com>
2023-05-18 15:22:04 -0400

    Updated Information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,13 +14,14 @@ export default function About(){
             <h4>hussam.baradei@yahoo.com</h4>
             <a href='https://drive.google.com/drive/folders/1QLBi5Xwqfq5fndMmuKvVJYlcsqORQEIn?usp=sharing' target="_blank"><h4>View Resume</h4></a>
             <h3>About me:</h3>
-        <h4>Hi, my name is Hussam Baradei I'm currently a second year student, studying Computer Science at York University</h4>
+        <h4>Hi, my name is Hussam Baradei I'm currently a third year student, studying Computer Science at York University</h4>
         <h3>Interests:</h3>
         <h4>Outside of my field of study I'm really interested in traveling. So far I’ve been to 2 out of the 7 wonders of the world.  My goal is to travel to all 7 throughout my lifetime. I also like to go out of my comfort zone and try new things, whether it's sky diving or something more laid back I'll give it a go</h4>
        
         
         <a href="https://www.linkedin.com/in/hussam-baradei-2b671423b/" target="_blank"><button className="linkedin">LinkedIn</button></a>
         <a href="https://github.com/Hussambx" target="_blank"><button className="viewgithub" >Github</button></a>
+        <a href="https://www.instagram.com/hussambaradei/?hl=en" target="_blank"><button className="viewig" >Instagram</button></a>
             </div>
 
    
--- src/components/Toolbox.jsx
+++ src/components/Toolbox.jsx
@@ -3,18 +3,19 @@ import React from "react";
 export default function Toolbox(){
     return(
         <>
-        <h2 className="toolboxtitle">Coding Languages:</h2>
+        <h2 className="toolboxtitle">Technical Skills:</h2>
         <div id="toolbox">
-       <h2>Java</h2>
-        <h2>Java Script </h2>
-        <h2>HTML/CSS</h2>
-        <h2>React</h2>
-        <h2>Node</h2>
-        <h2>Python</h2>
-        <h2>Mongo DB</h2>
-        <h2>Express</h2>
-        <h2>Lua</h2>
-        <h2>C#</h2>
+            <h3 id="toolboxtext">Languages: Java, Python, C/C++, Bash, JavaScript, HTML/CSS, Lua, Risc-V 
+            </h3>
+            <h3 id="toolboxtext">
+            Frameworks: React, Node.js, JUnit, AeroGameFramework
+            </h3>
+            <h3 id="toolboxtext">
+            Developer Tools: Git, Postmon, Mongo DB, Arduino, AWS, VS Code, FlyiO, IntelliJ, Eclipse, Unity, Roblox Studio
+            </h3>
+            <h3 id="toolboxtext">
+            Libraries: React Router, Express, Mongoose
+            </h3>
         </div>
         
         </>
